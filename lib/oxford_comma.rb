76d70517dbd251d9d3fa1aef5841ec9d599271c4
grace_aloo <graceaaloo@gmail.com>
2023-02-08 12:36:56 +0300

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    
+    if array.length == 1
+        return array[0] 
+    elsif array.length == 2
+         return array.join(" and ")
+    elsif array.length > 2
+        last_element = array.pop
+        return array.join(", ") + ", and " + last_element
+    end
+   
 end
